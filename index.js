8aaf749f60d790bc8309fb96328097e8fd4bc4d3
SAslam <soubhaan324@gmail.com>
2025-10-16 16:56:12 +0100

    Added HTML to include information about me.

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ var http = require("http");
 const port = 8000; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title><strong>My Page Server</strong></title>
+    </head>
+    <body>
+      <h1>About me</h1>
+      <p>My name is Soubhaan Aslam, third year student, haha im 6,3.</p>
+    </body>
+    </html>
+  `);
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 }); 
