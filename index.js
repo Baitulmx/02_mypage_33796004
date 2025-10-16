191e0084aaae4a58db2c3433fb3c474ea8e87c88
SAslam <soubhaan324@gmail.com>
2025-10-16 17:41:42 +0100

    Added background colour and centered header

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ http.createServer(function(req, res) {
     <html lang="en">
     <head>
       <meta charset="UTF-8">
-      <title><strong>My Page Server</strong></title>
+      <title>My Page Server</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #c6bbf3ff;
+        p {
+          font-size: 24px;
+        }
+        h1 {
+          text-align: center;
+          color: #6b6868ff;
+        }
+      </style>
     </head>
     <body>
       <h1>About me</h1>
